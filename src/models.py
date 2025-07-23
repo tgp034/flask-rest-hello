516dcdfa02d9d703767bec6620e7f1619f8bce06
Tito Julio González Padial <72378065+tgp034@users.noreply.github.com>
2025-07-23 23:17:45 +0000

    Serialize added

--- src/models.py
+++ src/models.py
@@ -15,10 +15,30 @@ class User(db.Model):
     followers: Mapped[list["Follower"]] = db.relationship('Follower', foreign_keys='Follower.user_to_id', backref='followed_user', lazy=True)
     following: Mapped[list["Follower"]] = db.relationship('Follower', foreign_keys='Follower.user_from_id', backref='follower_user', lazy=True)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'email': self.email,
+            'posts': [post.id for post in self.posts],
+            'followers': [follower.user_from_id for follower in self.followers],
+            'following': [follower.user_to_id for follower in self.following],
+            'comments': [comment.id for comment in self.comments],
+        }
+    
 class Follower(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_from_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
     user_to_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_from_id': self.user_from_id,
+            'user_to_id': self.user_to_id,
+        }
 
 class Post(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -26,12 +46,28 @@ class Post(db.Model):
     comments = db.relationship('Comment', backref='post', lazy=True)
     media = db.relationship('Media', backref='post', lazy=True)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'comments': [comment.id for comment in self.comments],
+            'media': [media.id for media in self.media],
+        }
+
 class Comment(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     comment_text: Mapped[str] = mapped_column(String(300), nullable=False)
     author_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
     post_id: Mapped[int] = mapped_column(db.ForeignKey('post.id'), nullable=False)
     
+    def serialize(self):
+        return {
+            'id': self.id,
+            'comment_text': self.comment_text,
+            'author_id': self.author_id,
+            'post_id': self.post_id,
+        }
+
 class Media(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     type: Mapped[enumerate] = mapped_column(String(50), nullable=False)
@@ -41,7 +77,8 @@ class Media(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            
-            # do not serialize the password, its a security breach
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id,
         }
+
